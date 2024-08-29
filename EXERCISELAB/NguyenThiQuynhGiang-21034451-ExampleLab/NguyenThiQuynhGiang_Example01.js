f621cb8c86fa1c7b79ea84f7bbacf5f1fc4d446b
Nguyen QuynhGiang <nguyenquynhgiang1324@gmail.com>
2024-08-29 23:49:51 +0700

    challenges_newbie_Part_01 number 01 02 03 04 05

---
+++ EXERCISELAB/NguyenThiQuynhGiang-21034451-ExampleLab/NguyenThiQuynhGiang_Example01.js
@@ -0,0 +1,8 @@
+/**
+ * Filter --> myFilter, find, every, some, map, reduce, any, forEach
+ */
+
+//1.Filter
+const ages = [4, 11, 42, 14, 39, 5];
+const filteredAge = ages.filter(ages => ages > 10);
+console.log(filteredAge);
