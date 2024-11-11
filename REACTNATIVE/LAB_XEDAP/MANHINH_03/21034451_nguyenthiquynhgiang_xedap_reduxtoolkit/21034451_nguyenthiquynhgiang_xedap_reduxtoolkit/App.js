23b4485a51f4b33ae51d5f55e85d1fe39ccfa9f0
Nguyen QuynhGiang <nguyenquynhgiang1324@gmail.com>
2024-11-11 16:30:58 +0700

    Screen03 + Minh Chung

---
+++ REACTNATIVE/LAB_XEDAP/MANHINH_03/21034451_nguyenthiquynhgiang_xedap_reduxtoolkit/21034451_nguyenthiquynhgiang_xedap_reduxtoolkit/App.js
@@ -0,0 +1,27 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+import Screen01 from './components/Screen01'
+import Screen02 from './components/Screen02'
+import Screen03 from './components/Screen03'
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux';
+import store from './components/store';
+
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    //<Screen02 />
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Screen02">
+          <Stack.Screen name="Screen01" component={Screen01} />
+          <Stack.Screen name="Screen02" component={Screen02} />
+          <Stack.Screen name="Screen03" component={Screen03} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+};
